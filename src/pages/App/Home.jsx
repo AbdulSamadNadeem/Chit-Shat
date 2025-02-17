d8e924ec873c2a4f114fa24036dcd104ac7c4577
AbdulSamadNadeem <samadnadeem372@gmail.com>
2025-02-18 04:19:47 +0500

    Home.jsx

--- src/pages/App/Home.jsx
+++ src/pages/App/Home.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import Header from "../../components/Header/Header";
 import Input from "../../components/Input/Input";
-import ChatRoom from "../../components/ChatRoom/ChatRoom";
+import ChatRoom from "../../components/ChatRoom/ChatRoom.jsx";
 import { io } from "socket.io-client";
 import Connecteduser from "../../components/ConnectedUser/Connecteduser";
 
