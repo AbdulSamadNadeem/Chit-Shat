ec45286b115b91912e62d436a28d9edb5bd50fd4
AbdulSamadNadeem <samadnadeem372@gmail.com>
2025-02-18 11:47:10 +0500

    Home.jsx

--- src/pages/App/Home.jsx
+++ src/pages/App/Home.jsx
@@ -4,7 +4,7 @@ import Header from "../../components/Header/Header";
 import Input from "../../components/Input/Input";
 import { io } from "socket.io-client";
 import Connecteduser from "../../components/ConnectedUser/Connecteduser";
-import Chatroom from "components/Chatroom/Chatroom";
+import Chatarea from "components/Chatroom/Chatroom";
 
 const Home = () => {
   const socket = io("http://127.0.0.1:3000");
@@ -21,7 +21,7 @@ const Home = () => {
           <Input />
           <Connecteduser />
         </div>
-        {selected && <Chatroom socket={socket} />}
+        {selected && <Chatarea socket={socket} />}
       </div>
     </div>
   );
